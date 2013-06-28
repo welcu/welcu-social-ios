e12d69a90c054253ee7acb5ec7bb068d3cb6a440
Seba Gamboa <me@sagmor.com>
2013-06-28 14:21:58 -0400

    Initial Commit

---
+++ Welcu Social/WelcuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WelcuAppDelegate.h
+//  Welcu Social
+//
+//  Created by Seba Gamboa on 6/28/13.
+//  Copyright (c) 2013 Welcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcuAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
