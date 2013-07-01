0c396788a9f95157794d9b29dedf3f579f8d6744
Seba Gamboa <me@sagmor.com>
2013-07-01 12:33:46 -0400

    Working on Photo compose

---
+++ Welcu Social/Controllers/WelcuComposePhotoCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcuComposePhotoCameraViewController.h
+//  Welcu Social
+//
+//  Created by Seba Gamboa on 6/30/13.
+//  Copyright (c) 2013 Welcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcuComposePhotoCameraViewController : UIViewController
+
+@end
